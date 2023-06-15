595258735b7e41d7d56717cdaba6ccf899fbbdfc
Alfatista2 <teteusoliveira9@gmail.com>
2023-06-14 21:59:24 -0300

    V0.1.5 - Color selector done

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+const theme_selector = document.querySelector(".theme_selector");
+
+document.onkeyup = function (event) {
+  if (event.key === "AltGraph" && event.ctrlKey) {
+    trocaCor();
+  }
+};
+
+function showSelector() {
+  theme_selector.style.transform = "translate(0%, -50%)";
+}
+
+function hideSelector() {
+  theme_selector.style.transform = "translate(100%, -50%)";
+}
+
+showSelector();
+setTimeout(hideSelector, 2000);
